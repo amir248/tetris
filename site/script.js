0daaaffa4daeec84f0ce0be0621124419311280f
chikchicly <chikchicly@gmail.com>
2023-05-01 18:28:01 +0700

    Go fuck yourself scum!

--- site/script.js
+++ site/script.js
@@ -12,8 +12,13 @@ let yr=document.createElement('p');
 yr.className='yr';
 document.querySelector('main').append(yr);
 
+let yy=document.createElement('p');
+yy.className='yy';
+document.querySelector('main').append(yy);
+
 let number=+0;
 let number3=+70;
+let fuck=-50;
 document.querySelector('main').style.cssText=`
 display:flex;
 justify-content:center;
@@ -24,17 +29,21 @@ width:100%;
 const lol=setInterval(()=>{
   number=number+10;
   number3=number3+10;
+  fuck=fuck+10;
   yu.style.cssText='top: '+`${number}`+'px';
+  yy.style.cssText='top: '+`${number}`+'px';
   yo.style.cssText='top: '+`${number3}`+'px';
   yl.style.cssText='top: '+`${number3}`+'px';
   yr.style.cssText='top: '+`${number3}`+'px';
-  console.log(window.innerHeight);
+  yy.style.cssText='top: '+`${fuck}`+'px;';
+  // console.log(window.innerHeight);
   if(window.innerHeight<number){
     console.log('Go fuck yourself scum!');
     yu.remove();
     yo.remove();
     yl.remove();
     yr.remove();
+    yy.remove();
     clearInterval(lol);
   }
 },300);
